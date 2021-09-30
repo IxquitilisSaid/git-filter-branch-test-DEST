32c7ed8fdb63dd6fdf1d9005608d50a2a432fec6
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2021-09-30 14:48:10 +0200

    1st dest commit

---
+++ src-up/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+// This is for the 4th commit OUOUOU
+// This is for the 6th commit OUOUOU
+// This is for the 1st DEST commit EYEYEYEYEY
